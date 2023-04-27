eff13577061dcf81eaa362e26df914dd41e4337c
AM07 <am.sathish07@gmail.com>
2023-04-27 21:42:21 +0530

    adding handlers

--- main.go
+++ main.go
@@ -1,38 +1,25 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
-	model "github.com/AM07/backend/models"
+	"github.com/AM07/backend/handlers"
 	"github.com/gorilla/mux"
 )
 
-var products []model.Product
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home page")
-	// fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	// http.ServeFile(w, r, "./static/home.html")
+func init() {
+	router()
 }
 
-func aboutPage(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintf(w, "About page")
-	a := model.Product{Name: "laptop", User: "AMS", Site: "www.amazon.com", Price: 98000}
-	products = append(products, a)
-	fmt.Fprintf(w, "Details of the server:\t")
-	json.NewEncoder(w).Encode(a)
-}
-
-func handlefunc() {
+func router() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", homePage)
-	r.HandleFunc("/about", aboutPage)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	r.HandleFunc("/", handlers.HomePage).Methods("GET")
+	r.HandleFunc("/about", handlers.AboutPage).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8081", r))
 }
 
 func main() {
-	handlefunc()
+	fmt.Println("Site running at http://localhost:8081...")
 }
