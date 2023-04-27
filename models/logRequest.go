896ac9d17e8691157c3ac92e3dc84e552a451d4f
AM07 <am.sathish07@gmail.com>
2023-04-27 23:30:23 +0530

    formatting code

---
+++ models/logRequest.go
@@ -0,0 +1,14 @@
+package model
+
+import "time"
+
+type HTTPReqInfo struct {
+	method    string
+	uri       string
+	referer   string
+	ipaddr    string
+	code      int
+	size      int64
+	duration  time.Duration
+	userAgent string
+}
